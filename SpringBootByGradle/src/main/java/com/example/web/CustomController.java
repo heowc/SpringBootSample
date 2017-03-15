dd2fa669a866fc990da21b1fcbebc5347ae2db75
heo won chul <heowc1992@gmail.com>
2017-03-15 14:09:45 +0900

    refectoring spring boot

--- SpringBootByGradle/src/main/java/com/example/web/CustomController.java
+++ SpringBootByGradle/src/main/java/com/example/web/CustomController.java
@@ -1,22 +1,19 @@
 package com.example.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.domain.Customer;
 import com.example.domain.CustomerRepository;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomController {
 
 	@Autowired
 	private CustomerRepository repository;
 	
-	@PostMapping(value="/insert",
+	@PostMapping(value="/",
 				produces="application/json")
 	public Customer insert(@RequestBody Customer customer){
 		return repository.save(customer);
