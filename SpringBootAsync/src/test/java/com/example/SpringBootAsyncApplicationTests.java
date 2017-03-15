8a590543f69e6abf96ca1ddf800e81e261b74354
heo won chul <heowc1992@gmail.com>
2017-03-15 14:25:19 +0900

    refectoring spring boot async

---
+++ SpringBootAsync/src/test/java/com/example/SpringBootAsyncApplicationTests.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import com.example.service.BasicService;
+import org.junit.Test;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StopWatch;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAsyncApplicationTests {
+
+	@Autowired BasicService service;
+
+	@Test
+	public void test_async() {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		service.onAsync();
+		stopWatch.stop();
+		System.out.println(stopWatch.toString());
+	}
+
+	@Test
+	public void test_sync() {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		service.onSync();
+		stopWatch.stop();
+		System.out.println(stopWatch.toString());
+	}
+}
