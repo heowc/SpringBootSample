7633b6f423d8f2a36d0ec285332b5c8f6ca54d97
heo won chul <heowc1992@gmail.com>
2017-03-22 09:08:54 +0900

    add time package issue

---
+++ SpringBootJpa/src/main/java/com/example/simple/web/TimeController.java
@@ -0,0 +1,17 @@
+package com.example.simple.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("date")
+public class TimeController {
+
+    @GetMapping
+    public LocalDateTime getLocalDateTime() {
+        return LocalDateTime.now();
+    }
+}
