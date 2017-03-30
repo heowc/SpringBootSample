8375010bdd4c52c4ed7af343194ad7a7e8cb0eb2
heo won chul <heowc1992@gmail.com>
2017-03-30 22:53:41 +0900

    refactoring spring boot social

---
+++ SpringBootSocial/src/main/java/com/example/config/SocialConfig.java
@@ -0,0 +1,9 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.social.config.annotation.EnableSocial;
+
+@Configuration
+@EnableSocial
+public class SocialConfig {
+}
--- SpringBootSocial/src/main/java/com/example/web/FacebookController.java
+++ SpringBootSocial/src/main/java/com/example/web/FacebookController.java
@@ -1,26 +1,23 @@
 package com.example.web;
 
 import com.example.social.facebook.FacebookUser;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/")
 public class FacebookController {
 
-	private Facebook facebook;
-	private ConnectionRepository connectionRepository;
+	@Autowired Facebook facebook;
+	@Autowired ConnectionRepository connectionRepository;
 
-	public FacebookController(Facebook facebook, ConnectionRepository connectionRepository) {
-		this.facebook = facebook;
-		this.connectionRepository = connectionRepository;
-	}
+	private static final Logger logger = Logger.getLogger(FacebookController.class);
 
 	@GetMapping
 	public String helloFacebook(Model model) {
@@ -28,9 +25,9 @@ public class FacebookController {
 			return "redirect:/connect/facebook";
 		}
 
-		FacebookUser facebookUser = facebook.fetchObject("me", FacebookUser.class);
+		FacebookUser facebookUser = facebook.fetchObject("/me", FacebookUser.class);
 
-//		System.out.println(facebookUser);
+		logger.info(facebookUser);
 
 		model.addAttribute("facebookProfile", facebookUser);
 		return "main";
