2af3302bcf9086bcaf19a7acc2c2c4c1097daeca
heo won chul <heowc1992@gmail.com>
2017-04-30 21:22:14 +0900

    add spring boot excel upload & download

---
+++ SpringBootExcel/src/main/java/com/titstory/heowc/component/ExcelXlsxView.java
@@ -0,0 +1,21 @@
+package com.titstory.heowc.component;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Component
+public class ExcelXlsxView extends AbstractXlsxView {
+
+    @Override
+    protected void buildExcelDocument(Map<String, Object> model,
+                                      Workbook workbook,
+                                      HttpServletRequest request,
+                                      HttpServletResponse response) throws Exception {
+
+    }
+}
