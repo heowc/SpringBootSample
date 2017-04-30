9cab5e6f25fef3f269ec2ba1a1f59a4800613165
heo won chul <heowc1992@gmail.com>
2017-05-01 08:51:33 +0900

    refectoring spring boot excel

--- SpringBootExcel/src/test/java/com/titstory/heowc/SpringBootExcelApplicationTests.java
+++ SpringBootExcel/src/test/java/com/titstory/heowc/SpringBootExcelApplicationTests.java
@@ -1,16 +1,31 @@
 package com.titstory.heowc;
 
+import com.titstory.heowc.component.ExcelReadComponent;
+import com.titstory.heowc.domain.Product;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootExcelApplicationTests {
 
+	@Autowired
+	ExcelReadComponent excelReadComponent;
+
 	@Test
-	public void contextLoads() {
+	public void test_readExcel() throws IOException, InvalidFormatException {
+		File xlsxFile = new File("{path}\\test.xlsx");
+
+		excelReadComponent
+				.readExcelToList(xlsxFile, Product::new)
+				.forEach(System.out::println);
 	}
 
 }
