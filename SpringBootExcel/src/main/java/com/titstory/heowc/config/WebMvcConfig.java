1699da694a263a70d1e03516c151a52e339b44bd
heo won chul <heowc1992@gmail.com>
2017-04-30 21:22:24 +0900

    add spring boot excel upload & download

---
+++ SpringBootExcel/src/main/java/com/titstory/heowc/config/WebMvcConfig.java
@@ -0,0 +1,9 @@
+package com.titstory.heowc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+}
