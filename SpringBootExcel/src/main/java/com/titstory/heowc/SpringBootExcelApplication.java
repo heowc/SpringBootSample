7ec44121a3379f7b0b8e85d0e78d6eb462b55aa8
heo won chul <heowc1992@gmail.com>
2017-04-30 21:23:10 +0900

    add spring boot excel upload & download

---
+++ SpringBootExcel/src/main/java/com/titstory/heowc/SpringBootExcelApplication.java
@@ -0,0 +1,12 @@
+package com.titstory.heowc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExcelApplication.class, args);
+	}
+}
