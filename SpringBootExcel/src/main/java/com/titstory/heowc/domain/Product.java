3b08adba1d52593422f526e69835443239f3d9b2
heo won chul <heowc1992@gmail.com>
2017-05-01 08:50:52 +0900

    refectoring spring boot excel

--- SpringBootExcel/src/main/java/com/titstory/heowc/domain/Product.java
+++ SpringBootExcel/src/main/java/com/titstory/heowc/domain/Product.java
@@ -2,6 +2,7 @@ package com.titstory.heowc.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.poi.ss.usermodel.Row;
 
 import java.io.Serializable;
 
@@ -15,4 +16,9 @@ public class Product implements Serializable {
 
     private String comment;
 
+    public Product(Row row) {
+        this(row.getCell(0).getStringCellValue(),
+                    row.getCell(1).getStringCellValue(),
+                    row.getCell(2).getStringCellValue());
+    }
 }
