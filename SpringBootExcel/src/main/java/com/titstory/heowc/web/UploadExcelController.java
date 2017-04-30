61ee7e67a9ce2602f7df5b9334fbedfe164398aa
heo won chul <heowc1992@gmail.com>
2017-05-01 08:50:45 +0900

    refectoring spring boot excel

---
+++ SpringBootExcel/src/main/java/com/titstory/heowc/web/UploadExcelController.java
@@ -0,0 +1,30 @@
+package com.titstory.heowc.web;
+
+import com.titstory.heowc.component.ExcelReadComponent;
+import com.titstory.heowc.domain.Product;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("upload")
+public class UploadExcelController {
+
+    @Autowired ExcelReadComponent excelReadComponent;
+
+    @PostMapping("excel")
+    public List<Product> readExcel(@RequestParam("file") MultipartFile multipartFile)
+                                                throws IOException, InvalidFormatException {
+        return excelReadComponent
+                .readExcelToList(new File(multipartFile.getOriginalFilename()),
+                                    Product::new);
+    }
+}
