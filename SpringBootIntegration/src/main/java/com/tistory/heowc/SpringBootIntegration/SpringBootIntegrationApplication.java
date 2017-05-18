a0d5da57d12d0871a038a3e3587cfe12d8de093b
heo won chul <heowc1992@gmail.com>
2017-05-18 10:49:56 +0900

    add spring-boot-integration

---
+++ SpringBootIntegration/src/main/java/com/tistory/heowc/SpringBootIntegration/SpringBootIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.tistory.heowc.SpringBootIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntegrationApplication.class, args);
+	}
+}
