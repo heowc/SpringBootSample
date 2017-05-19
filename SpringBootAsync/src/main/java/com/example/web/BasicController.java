2437e263e68b9073b982c7acdcfcd56eacff404f
heo won chul <heowc1992@gmail.com>
2017-05-19 18:04:00 +0900

    add refactoring

--- SpringBootAsync/src/main/java/com/example/web/BasicController.java
+++ SpringBootAsync/src/main/java/com/example/web/BasicController.java
@@ -1,18 +1,22 @@
 package com.example.web;
 
+import com.example.service.BasicService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.service.BasicService;
+import org.springframework.web.client.AsyncRestTemplate;
 
 @RestController
 public class BasicController {
 
-	@Autowired
-	private BasicService service;
-	
+	@Autowired BasicService service;
+
+	@Autowired AsyncRestTemplate asyncRestTemplate;
+
 	private static final Logger logger = Logger.getLogger(BasicController.class);
 	
 	@GetMapping("/async")
@@ -31,5 +35,19 @@ public class BasicController {
 		logger.info(str);
 		logger.info("==================================");
 		return str;
+	}
+
+	@GetMapping("/{userName}")
+	public void getRepository(@PathVariable String userName) {
+		getRepositoryInfo(userName)
+				.addCallback(
+						System.out::println,
+						System.out::println
+				);
+	}
+
+	private ListenableFuture<ResponseEntity<String>> getRepositoryInfo(String userName) {
+		return asyncRestTemplate
+				.getForEntity("https://api.github.com/users/" + userName + "/repos", String.class);
 	}
 }
