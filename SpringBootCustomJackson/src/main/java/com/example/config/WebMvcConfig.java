2c7ffeff03833280a0086d641ca83223b2819635
heo won chul <heowc1992@gmail.com>
2017-05-26 09:53:37 +0900

    refactoring

--- SpringBootCustomJackson/src/main/java/com/example/config/WebMvcConfig.java
+++ SpringBootCustomJackson/src/main/java/com/example/config/WebMvcConfig.java
@@ -6,10 +6,10 @@ import com.example.domain.Model;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -22,22 +22,18 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	
 	private final DecyptDataDeserializer deserializer;
 	private final EncyptDataSerializer serializer;
-	private final CustomJsonMapper mapper;
 
 	@Override
 	public void configureMessageConverters(
 			List<HttpMessageConverter<?>> converters) {
-		converters.add(new MappingJackson2HttpMessageConverter(mapper));
+		converters.add(new MappingJackson2HttpMessageConverter(getObjectMapper()));
 	}
 
-	@Component
-	class CustomJsonMapper extends ObjectMapper {
-		
-		public CustomJsonMapper() {
-			SimpleModule module = new SimpleModule();
-			module.addSerializer(Model.class, serializer);
-			module.addDeserializer(Model.class, deserializer);
-			registerModule(module);
-		}
+	@Bean
+	public ObjectMapper getObjectMapper() {
+		SimpleModule module = new SimpleModule();
+		module.addSerializer(Model.class, serializer);
+		module.addDeserializer(Model.class, deserializer);
+		return new ObjectMapper().registerModule(module);
 	}
 }
