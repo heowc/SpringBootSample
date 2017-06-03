89c5c80acb157106ef88282a22a22ac633b859dd
heo won chul <heowc1992@gmail.com>
2017-06-03 11:58:21 +0900

    add spring boot redis

---
+++ SpringBootRedis/src/main/java/com/tistory/heowc/SpringBootRedisApplication.java
@@ -0,0 +1,12 @@
+package com.tistory.heowc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisApplication.class, args);
+	}
+}
