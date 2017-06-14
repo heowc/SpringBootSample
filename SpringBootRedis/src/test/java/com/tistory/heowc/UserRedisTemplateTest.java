075e1f6d2859283df4df334d3e9b73e1564ed513
heo won chul <heowc1992@gmail.com>
2017-06-14 11:02:30 +0900

    refactoring redis

--- SpringBootRedis/src/test/java/com/tistory/heowc/UserRedisTemplateTest.java
+++ SpringBootRedis/src/test/java/com/tistory/heowc/UserRedisTemplateTest.java
@@ -2,18 +2,14 @@ package com.tistory.heowc;
 
 import com.tistory.heowc.domain.User;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.util.Lists;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,17 +19,15 @@ public class UserRedisTemplateTest {
 
     @Autowired RedisTemplate<String, User> userRedisTemplate;
 
-    @Resource(name = "userRedisTemplate") ListOperations<String, User> listOps;
-
     @Test
-    public void test1_push() throws Exception {
-        listOps.leftPush("user", User.getDefaultUser());
+    public void test1_set() throws Exception {
+        userRedisTemplate.opsForValue().set("user", User.getDefaultUser());
     }
 
     @Test
-    public void test1_pop() throws Exception {
+    public void test2_get() throws Exception {
         log.info(
-                listOps.rightPop("user").toString()
+                userRedisTemplate.opsForValue().get("user").toString()
         );
     }
 }
