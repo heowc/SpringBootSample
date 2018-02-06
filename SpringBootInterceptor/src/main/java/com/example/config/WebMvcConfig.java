e5b90959076ffd031754ebb7d2f01d536e7a0ce6
heowc <heowc1992@gmail.com>
2018-02-06 21:36:40 +0900

    Fix: [interceptor] refactoring - fix config

--- SpringBootInterceptor/src/main/java/com/example/config/WebMvcConfig.java
+++ SpringBootInterceptor/src/main/java/com/example/config/WebMvcConfig.java
@@ -1,20 +1,18 @@
 package com.example.config;
 
-import com.example.component.HttpInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-	private final HttpInterceptor interceptor;
-
 	@Autowired
-	public WebMvcConfig(HttpInterceptor interceptor) {
-		this.interceptor = interceptor;
-	}
+	@Qualifier(value = "httpInterceptor")
+	private HandlerInterceptor interceptor;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
