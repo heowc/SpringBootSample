2f0c9d3d9f0b8c529185385a0aac4247e0d8c09a
heowc <heowc1992@gmail.com>
2018-03-07 20:43:16 +0900

    Fix: [jackson] refactoring - fix version(2.0.0.RELEASE) - formatting - JsonComponent

--- SpringBootCustomJackson/src/main/java/com/example/domain/Model.java
+++ SpringBootCustomJackson/src/main/java/com/example/domain/Model.java
@@ -1,25 +1,33 @@
 package com.example.domain;
 
-import java.io.Serializable;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@SuppressWarnings({ "unused", "serial" })
-@Getter @Setter
-public class Model implements Serializable {
+public class Model {
 
 	private String name;
 	private int type;
+
+	protected Model() { }
 
 	public Model(String name, int type) {
 		this.name = name;
 		this.type = type;
 	}
-	
-	private Model() {
+
+	public String getName() {
+		return name;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
 	@Override
 	public String toString() {
 		return "Model [name=" + name + ", type=" + type + "]";
--- SpringBootCustomJackson/src/main/java/com/example/web/BasicController.java
+++ SpringBootCustomJackson/src/main/java/com/example/web/BasicController.java
@@ -1,13 +1,12 @@
 package com.example.web;
 
-import java.util.ArrayList;
-
+import com.example.domain.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.domain.Model;
+import java.util.ArrayList;
 
 @RestController
 public class BasicController {
@@ -15,22 +14,22 @@ public class BasicController {
 	@GetMapping("/se")
 	public ArrayList<Model> index() {
 		ArrayList<Model> list = new ArrayList<Model>();
-		list.add(new Model("abc",1));
-		list.add(new Model("def",2));
-		list.add(new Model("ghi",3));
-		list.add(new Model("jkl",4));
-		list.add(new Model("nmo",5));
-		list.add(new Model("pqr",6));
-		list.add(new Model("stu",7));
-		list.add(new Model("vwx",8));
-		list.add(new Model("yz",9));
-		
-		return list; 
+		list.add(new Model("abc", 1));
+		list.add(new Model("def", 2));
+		list.add(new Model("ghi", 3));
+		list.add(new Model("jkl", 4));
+		list.add(new Model("nmo", 5));
+		list.add(new Model("pqr", 6));
+		list.add(new Model("stu", 7));
+		list.add(new Model("vwx", 8));
+		list.add(new Model("yz", 9));
+
+		return list;
 	}
-	
+
 	@PostMapping("/de")
 	public ArrayList<Model> deIndex(@RequestBody ArrayList<Model> deList) {
 		System.out.println(deList);
-		return deList; 
+		return deList;
 	}
 }
