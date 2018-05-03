38dc837a426b8453fdcdee0dee06c3ab212df186
heowc <heowc1992@gmail.com>
2018-05-03 22:04:41 +0900

    Add: [spring-batch] - add config

--- SpringBootVertx/src/main/java/com/tistory/heowc/repository/ArticleRepository.java
+++ SpringBootVertx/src/main/java/com/tistory/heowc/repository/ArticleRepository.java
@@ -2,7 +2,6 @@ package com.tistory.heowc.repository;
 
 import com.tistory.heowc.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
