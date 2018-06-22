f33dd105ea8f14643d68c2dd53e2485cc00858b5
heowc <heowc1992@gmail.com>
2018-06-22 15:40:25 +0900

    Fix: [spring-jpa] - refactoring

--- SpringBootJpa/src/test/java/com/example/CustomerRepositoryTests.java
+++ SpringBootJpa/src/test/java/com/example/CustomerRepositoryTests.java
@@ -2,45 +2,34 @@ package com.example;
 
 import com.example.simple.domain.Customer;
 import com.example.simple.repository.CustomerRepository;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@Transactional
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CustomerRepositoryTests {
 
-	@Autowired
-	private CustomerRepository repository;
-	@Autowired
-	private TestEntityManager testEntityManager;
+    @Autowired
+    private CustomerRepository repository;
+    @Autowired
+    private TestEntityManager testEntityManager;
 
-	// 비영속성 데이터
-	private Customer getPersistenceContextCustomer() {
-		return new Customer("heo won chul", "010-xxxx-xxxx", "developer");
-	}
+    // 비영속성 데이터
+    private Customer getPersistenceContextCustomer() {
+        return new Customer("heo won chul", "010-xxxx-xxxx", "developer");
+    }
 
-	// 준영속성 데이터
-	private Customer getNotPersistenceContextCustomer() {
-		return new Customer("heo won chul", "010-xxxx-xxxx", "developer");
-	}
+    @Test
+    public void test_update() {
+        Customer customer = repository.save(getPersistenceContextCustomer());
+        customer.changeBigo("Developer");
 
-	@Test
-	public void test_update() {
-		Customer customer = repository.save(getPersistenceContextCustomer());
-		customer.changeBigo("Developer");
-
-		assertNotEquals(repository.save(customer).getBigo(), getNotPersistenceContextCustomer().getBigo());
-		testEntityManager.flush();
-	}
+        assertNotEquals(repository.save(customer).getBigo(), "developer");
+    }
 }
--- SpringBootJpa/src/test/java/com/example/EntityManagerTests.java
+++ SpringBootJpa/src/test/java/com/example/EntityManagerTests.java
@@ -7,56 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class EntityManagerTests {
 
-	@Autowired
-	TestEntityManager testEntityManager;
+    @Autowired
+    private TestEntityManager testEntityManager;
 
-	// 비영속성 데이터
-	private Customer getPersistenceContextCustomer() {
-		return new Customer("heo won chul", "010-xxxx-xxxx", "developer");
-	}
+    // 비영속성 데이터
+    private Customer getPersistenceContextCustomer() {
+        return new Customer("heo won chul", "010-xxxx-xxxx", "developer");
+    }
 
-	// 준영속성 데이터
-	private Customer getNotPersistenceContextCustomer() {
-		return new Customer("heo won chul", "010-xxxx-xxxx", "developer");
-	}
+    @Test
+    public void test_insertClearAndFindAndUpdateClear() {
+        Customer customer = testEntityManager.persistFlushFind(getPersistenceContextCustomer());
+        customer.changeBigo("Developer");
+        testEntityManager.flush(); // Database 동기화
+//		  testEntityManager.clear(); // Persistence Context 초기화
 
-	@Transactional
-	@Test
-	public void test_insertA() {
-//        org.hibernate.PersistentObjectException: detached entity passed to persist
-//        testEntityManager.persist(getNotPersistenceContextCustomer());
-		testEntityManager.persist(getPersistenceContextCustomer());
-		testEntityManager.flush();
-	}
-
-	@Transactional
-	@Test
-	public void test_insertB() {
-		testEntityManager.merge(getNotPersistenceContextCustomer());
-		testEntityManager.flush();
-	}
-
-	@Transactional
-	@Test
-	public void test_insertClearAndFindAndUpdateClear() {
-		testEntityManager.merge(getNotPersistenceContextCustomer()); // Persistence Context 추가
-		testEntityManager.flush(); // Database 동기화
-//		testEntityManager.clear(); // Persistence Context 초기화
-
-		Customer customer = testEntityManager.find(Customer.class, 1L);
-		System.out.println(customer);
-		customer.changeBigo("Developer");
-		testEntityManager.merge(customer); // Persistence Context 추가
-		testEntityManager.flush(); // Database 동기화
-//		testEntityManager.clear(); // Persistence Context 초기화
-
-		Customer result = testEntityManager.find(Customer.class, 1L);
-		System.out.println(result);
-	}
+        Customer result = testEntityManager.find(Customer.class, customer.getIdx());
+        assertNotEquals(result.getBigo(), "developer");
+    }
 }
