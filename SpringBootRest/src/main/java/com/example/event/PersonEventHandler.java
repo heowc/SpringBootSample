e67d53e186c7de5efc93893d367e91b67e233f5d
heowc <heowc1992@gmail.com>
2018-10-31 21:13:12 +0900

    [FIX] spring boot rest - version up (1.5.2 -> 2.0.0) - apache log -> slf4j log 변경

--- SpringBootRest/src/main/java/com/example/event/PersonEventHandler.java
+++ SpringBootRest/src/main/java/com/example/event/PersonEventHandler.java
@@ -1,7 +1,8 @@
 package com.example.event;
 
 import com.example.domain.Person;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
 @RepositoryEventHandler(Person.class)
 public class PersonEventHandler {
 
-	private static final Logger logger = Logger.getLogger(PersonEventHandler.class);
+	private static final Logger logger = LoggerFactory.getLogger(PersonEventHandler.class);
 	
 	@HandleBeforeCreate
 	public void handlePersonCreate(Person p){
