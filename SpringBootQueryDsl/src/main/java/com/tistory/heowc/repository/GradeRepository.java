d1e717ec9719b6fd63d9f772ed19f404b3213ca9
heowc <heowc1992@gmail.com>
2018-11-01 11:52:33 +0900

    [FIX] spring boot querydsl - version up (1.5.2 -> 2.0.0) - 불필요한 빌드 설정 및 설정 제거 - 네이밍 변경된 클래스 수정

--- SpringBootQueryDsl/src/main/java/com/tistory/heowc/repository/GradeRepository.java
+++ SpringBootQueryDsl/src/main/java/com/tistory/heowc/repository/GradeRepository.java
@@ -1,7 +1,7 @@
 package com.tistory.heowc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.tistory.heowc.domain.Grade;
@@ -9,6 +9,6 @@ import com.tistory.heowc.domain.Grade;
 @Repository
 public interface GradeRepository extends JpaRepository<Grade, Integer>, 
 											GradeRepositoryCustom,
-											QueryDslPredicateExecutor<Grade> {
+		QuerydslPredicateExecutor<Grade> {
 
 }
--- SpringBootQueryDsl/src/main/java/com/tistory/heowc/repository/StudentRepository.java
+++ SpringBootQueryDsl/src/main/java/com/tistory/heowc/repository/StudentRepository.java
@@ -1,7 +1,7 @@
 package com.tistory.heowc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.tistory.heowc.domain.Student;
@@ -9,6 +9,6 @@ import com.tistory.heowc.domain.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer>,
 											StudentRepositoryCustom,
-											QueryDslPredicateExecutor<Student> {
+											QuerydslPredicateExecutor<Student> {
 
 }
