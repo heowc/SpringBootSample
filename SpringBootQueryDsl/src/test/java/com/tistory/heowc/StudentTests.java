748db4a52babf7dbb7ccaeca9a494fe59ae20d20
heowc <heowc1992@gmail.com>
2018-11-01 14:56:01 +0900

    [FIX] spring boot querydsl - 테스트 케이스 재작성

--- SpringBootQueryDsl/src/test/java/com/tistory/heowc/StudentTests.java
+++ SpringBootQueryDsl/src/test/java/com/tistory/heowc/StudentTests.java
@@ -1,9 +1,11 @@
 package com.tistory.heowc;
 
+import com.querydsl.core.Tuple;
 import com.tistory.heowc.domain.Grade;
 import com.tistory.heowc.domain.Student;
 import com.tistory.heowc.repository.GradeRepository;
 import com.tistory.heowc.repository.StudentRepository;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,15 +14,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 public class StudentTests {
-
-	@PersistenceContext EntityManager entityManager;
 
 	@Autowired StudentRepository studentRepository;
 	
@@ -29,79 +30,114 @@ public class StudentTests {
 	@Before
 	public void beforeTest() {
 		System.out.println("============================== Before");
-		Grade firstGrade = Grade.of(1, "일학년");
-		Grade secondGrade = Grade.of(2, "이학년");
-		Grade thirdGrade = Grade.of(3, "삼학년");
+
+		Grade firstGrade = gradeRepository.save(Grade.of(1,"일학년"));
+		Grade secondGrade = gradeRepository.save(Grade.of(2, "이학년"));
+		Grade thirdGrade = gradeRepository.save(Grade.of(3, "삼학년"));
 
 		Student wonchul = Student.of("wonchul", 173.8);
 		Student naeun = Student.of("naeun", 165.2);
 		Student tistory = Student.of("tistory", 160.0);
 
-		firstGrade.getStudents().add(wonchul);
-		secondGrade.getStudents().add(naeun);
-		thirdGrade.getStudents().add(tistory);
-
-		gradeRepository.save(firstGrade);
-		gradeRepository.save(secondGrade);
-		gradeRepository.save(thirdGrade);
+		wonchul.setGrade(firstGrade);
+		naeun.setGrade(secondGrade);
+		tistory.setGrade(thirdGrade);
 
 		studentRepository.save(wonchul);
 		studentRepository.save(naeun);
 		studentRepository.save(tistory);
 
-		entityManager.flush();
+		studentRepository.flush();
 	}
 	
 	@Test
 	public void test_findAllStudent() {
-		System.out.println("============================== findAllStudent");
-		studentRepository.findAll()
-							.forEach(System.out::println);
+		// given
+
+		// when
+		List<Student> studentList = studentRepository.findAll();
+
+		// then
+		assertThat(studentList).size().isEqualTo(3);
 	}
 	
 	@Test
 	public void test_findStudentByName() {
-		System.out.println("============================== findStudentByName");
-		studentRepository.findStudentByName("wonchul")
-							.forEach(System.out::println);
-		
-		studentRepository.findAll(studentRepository.equalName("wonchul"))
-							.forEach(System.out::println);
-		
-		studentRepository.findStudentByNameExtension("wonchul")
-							.forEach(System.out::println);
+		// given
+
+		// when
+		List<Student> studentList = studentRepository.findStudentByName("wonchul");
+		Iterable<Student> studentIterable = studentRepository.findAll(studentRepository.equalName("wonchul"));
+		List<Student> studentList2 = studentRepository.findStudentByNameExtension("wonchul");
+
+		// then
+		assertThat(studentList).element(0).satisfies(s -> {
+			Student student = studentIterable.iterator().next();
+
+			assertThat(s.getName()).isEqualTo(student.getName());
+			assertThat(s.getName()).isEqualTo(studentList2.get(0).getName());
+
+			assertThat(s.getHeight()).isEqualTo(student.getHeight());
+			assertThat(s.getHeight()).isEqualTo(studentList2.get(0).getHeight());
+		});
 	}
 	
 	@Test
 	public void test_findStudentByNameAndHeight() {
-		System.out.println("============================== test_findStudentByNameAndHeight");
-		studentRepository.findStudentByNameAndHeight("wonchul", 165.0)
-							.forEach(System.out::println);
+		// given
+		Student student = Student.of("wonchul", 165.0);
+
+		// when
+		List<Student> studentList = studentRepository.findStudentByNameAndHeight(student.getName(), student.getHeight());
+
+		// then
+		assertThat(studentList).size().isEqualTo(1);
+		assertThat(studentList).element(0).satisfies(s -> {
+			assertThat(s.getName()).isEqualTo(student.getName());
+			assertThat(s.getHeight()).isGreaterThanOrEqualTo(student.getHeight());
+			assertThat(s.getGrade().getGradeNum()).isEqualTo(1);
+			assertThat(s.getGrade().getGradeName()).isEqualTo("일학년");
+		});
 	}
 	
 	@Test
 	public void test_findStudentOrderByName() {
-		System.out.println("============================== findStudentOrderByName");
-		studentRepository.findStudentOrderByName()
-							.forEach(System.out::println);
+		// given
+
+		// when
+		List<Student> studentList = studentRepository.findStudentOrderByName();
+
+		// then
+		assertThat(studentList).element(0).satisfies(s -> assertThat(s.getName()).isEqualTo("naeun"));
+		assertThat(studentList).element(1).satisfies(s -> assertThat(s.getName()).isEqualTo("tistory"));
+		assertThat(studentList).element(2).satisfies(s -> assertThat(s.getName()).isEqualTo("wonchul"));
 	}
 	
 	@Test
 	public void test_findStudentGroupingByGradeNum() {
-		System.out.println("============================== findStudentGroupingByGradeNum");
-		studentRepository.findStudentGroupingByGradeNum()
-							.stream()
-							.map(gradeNum -> "학년 : " + gradeNum)
-							.forEach(System.out::println);
+		// given
+
+		// when
+		List<Integer> list = studentRepository.findStudentGroupingByGradeNum();
+
+		// then
+		assertThat(list).contains(1, 2, 3);
 	}
 	
 	@Test
 	public void test_findCaseStudentAll() {
-		System.out.println("============================== findCaseStudentAll");
-		studentRepository.findCaseStudentAll_Tuple()
-							.forEach(System.out::println);
-		
-//		studentRepository.findCaseStudentAll_Student()
-//							.forEach(System.out::println);
+		// given
+
+		// when
+		List<Tuple> studentList = studentRepository.findCaseStudentAll_Tuple();
+
+		// then
+		assertThat(studentList).allSatisfy(s -> assertThat(s.get(0, String.class)).isEqualTo("저학년"));
+	}
+
+	@After
+	public void after_clear() {
+		studentRepository.deleteAll();
+		gradeRepository.deleteAll();
 	}
 }
