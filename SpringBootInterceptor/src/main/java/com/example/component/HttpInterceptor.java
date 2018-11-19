80800b6c69bee25366b7392e43fd1ed79d38fda7
heowc <heowc1992@gmail.com>
2018-11-19 13:14:04 +0900

    [FIX] HandlerInterceptor로 변경

--- SpringBootInterceptor/src/main/java/com/example/component/HttpInterceptor.java
+++ SpringBootInterceptor/src/main/java/com/example/component/HttpInterceptor.java
@@ -3,39 +3,39 @@ package com.example.component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
-public class HttpInterceptor extends HandlerInterceptorAdapter {
+public class HttpInterceptor implements HandlerInterceptor {
 
-	private static final Logger logger = LoggerFactory.getLogger(HttpInterceptor.class);
+    private static final Logger logger = LoggerFactory.getLogger(HttpInterceptor.class);
 
-	@Override
-	public boolean preHandle(HttpServletRequest request,
-	                         HttpServletResponse response,
-	                         Object handler) {
-		logger.info("================ Before Method");
-		return true;
-	}
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) {
+        logger.info("================ Before Method");
+        return true;
+    }
 
-	@Override
-	public void postHandle(HttpServletRequest request,
-	                       HttpServletResponse response,
-	                       Object handler,
-	                       ModelAndView modelAndView) {
-		logger.info("================ Method Executed");
-		response.getHeaders("interceptor").add("interceptor");
-	}
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler,
+                           ModelAndView modelAndView) {
+        logger.info("================ Method Executed");
+        response.getHeaders("interceptor").add("interceptor");
+    }
 
-	@Override
-	public void afterCompletion(HttpServletRequest request,
-	                            HttpServletResponse response,
-	                            Object handler,
-	                            Exception ex) {
-		logger.info("================ Method Completed");
-	}
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception ex) {
+        logger.info("================ Method Completed");
+    }
 }
