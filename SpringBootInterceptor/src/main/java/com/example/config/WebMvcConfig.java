473bff22d3e32d00b57b05112d90e77834d02621
heowc <heowc1992@gmail.com>
2018-11-19 11:06:13 +0900

    [FIX] WebMvcConfigurer 변경
    Resolved: #3

--- SpringBootInterceptor/src/main/java/com/example/config/WebMvcConfig.java
+++ SpringBootInterceptor/src/main/java/com/example/config/WebMvcConfig.java
@@ -5,10 +5,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Autowired
 	@Qualifier(value = "httpInterceptor")
--- SpringBootNotice/src/main/java/com/tistory/heowc/config/WebMvcConfig.java
+++ SpringBootNotice/src/main/java/com/tistory/heowc/config/WebMvcConfig.java
@@ -2,10 +2,10 @@ package com.tistory.heowc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 	
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
 		"classpath:/static/"};
