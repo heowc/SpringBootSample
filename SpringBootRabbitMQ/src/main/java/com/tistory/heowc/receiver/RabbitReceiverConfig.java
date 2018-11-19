8357db3fa6245373d1a799086efbdb1a6fd42c80
heowc <heowc1992@gmail.com>
2018-11-19 16:24:23 +0900

    [FIX] 불필요한 RabbitListenerConfigurer 설정 제거

--- SpringBootRabbitMQ/src/main/java/com/tistory/heowc/receiver/RabbitReceiverConfig.java
+++ SpringBootRabbitMQ/src/main/java/com/tistory/heowc/receiver/RabbitReceiverConfig.java
@@ -2,18 +2,13 @@ package com.tistory.heowc.receiver;
 
 import com.tistory.heowc.common.Constat;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
-import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
 
 @Configuration
-public class RabbitReceiverConfig implements RabbitListenerConfigurer {
+public class RabbitReceiverConfig {
 
-    public static final String TOPIC_EXCHANGE_NAME = Constat.QUEUE_NAME + "-exchange";
+    private static final String TOPIC_EXCHANGE_NAME = Constat.QUEUE_NAME + "-exchange";
 
     @Bean
     public Queue queue() {
@@ -28,17 +23,5 @@ public class RabbitReceiverConfig implements RabbitListenerConfigurer {
     @Bean
     public Binding binding(Queue queue, TopicExchange topicExchange) {
         return BindingBuilder.bind(queue).to(topicExchange).with(Constat.QUEUE_NAME);
-    }
-
-    @Override
-    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
-        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
-    }
-
-    @Bean
-    MessageHandlerMethodFactory messageHandlerMethodFactory() {
-        DefaultMessageHandlerMethodFactory messageHandlerMethodFactory = new DefaultMessageHandlerMethodFactory();
-        messageHandlerMethodFactory.setMessageConverter(new MappingJackson2MessageConverter());
-        return messageHandlerMethodFactory;
     }
 }
