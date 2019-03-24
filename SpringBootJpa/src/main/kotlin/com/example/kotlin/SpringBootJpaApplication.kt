677a304e5d6b255b4ca5a1bd2ce7915de8fca2f9
heowc <heowc1992@gmail.com>
2019-03-24 23:43:13 +0900

    [FIX] #26 rename

--- SpringBootJpa/src/main/kotlin/com/example/kotlin/SpringBootJpaApplication.kt
+++ SpringBootJpa/src/main/kotlin/com/example/kotlin/SpringBootJpaApplication.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootJpaKotlinApplication {
+class SpringBootJpaApplication {
     fun main(args: Array<String>) {
-        runApplication<SpringBootJpaKotlinApplication>(*args)
+        runApplication<SpringBootJpaApplication>(*args)
     }
 }
 
