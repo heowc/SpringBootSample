c8ce5bdc8b37f6791f175119ef6ade43db168333
heowc <heowc1992@gmail.com>
2019-03-25 21:39:42 +0900

    [FIX] #26 SpringBootJpa

--- SpringBootJpa/src/main/kotlin/com/example/kotlin/SpringBootJpaApplication.kt
+++ SpringBootJpa/src/main/kotlin/com/example/kotlin/SpringBootJpaApplication.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootJpaApplication {
-    fun main(args: Array<String>) {
-        runApplication<SpringBootJpaApplication>(*args)
-    }
+class SpringBootJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootJpaApplication>(*args)
 }
 
