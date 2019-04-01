34b1d4af17124a3016b61dbfb9a25aa2eeb84715
heowc <heowc1992@gmail.com>
2019-04-01 23:49:09 +0900

    [FIX] etc

--- SpringBootVertx/src/main/java/com/tistory/heowc/verticle/ArticleRecipientVerticle.java
+++ SpringBootVertx/src/main/java/com/tistory/heowc/verticle/ArticleRecipientVerticle.java
@@ -8,16 +8,17 @@ import io.vertx.core.Handler;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.Json;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@Slf4j
 public class ArticleRecipientVerticle extends AbstractVerticle {
 
     public static final String GET_ALL_ARTICLES = "get.articles.all";
 
+    private static final Logger logger = LoggerFactory.getLogger(ArticleRecipientVerticle.class);
     private static final ObjectMapper mapper = Json.mapper;
 
     @Autowired
@@ -39,7 +40,7 @@ public class ArticleRecipientVerticle extends AbstractVerticle {
             try {
                 future.complete(mapper.writeValueAsString(service.getAllArticle()));
             } catch (JsonProcessingException e) {
-                log.error("Failed to serialize result");
+                logger.error("Failed to serialize result");
                 future.fail(e);
             }
         }, result -> {
