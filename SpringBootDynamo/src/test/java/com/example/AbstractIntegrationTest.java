c6a0aa7a2dca297dded40575c56ebc82bae6d579
heowc <heowc1992@gmail.com>
2020-03-29 15:59:27 +0900

    Apply `@DynamicPropertySource` from `spring-boot-dynamo`

--- SpringBootDynamo/src/test/java/com/example/AbstractIntegrationTest.java
+++ SpringBootDynamo/src/test/java/com/example/AbstractIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -8,18 +10,20 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers(disabledWithoutDocker = true)
 public abstract class AbstractIntegrationTest {
 
-    private static final String DOCKER_IMAGE = "amazon/dynamodb-local";
-    private static final String DOCKER_TAG = "latest";
-    private static final int EXPOSED_PORT = 8000;
+    private static final GenericContainer dynamodb =
+            new GenericContainer("amazon/dynamodb-local:latest").withExposedPorts(8000);
 
     static {
-        GenericContainer dynamodb =
-                new GenericContainer(String.format("%s:%s", DOCKER_IMAGE, DOCKER_TAG)).withExposedPorts(EXPOSED_PORT);
         dynamodb.start();
+    }
 
+    @DynamicPropertySource
+    static void dynamoDbProperties(DynamicPropertyRegistry registry) {
         final String endpoint = String.format("http://%s:%s",
                 dynamodb.getContainerIpAddress(),
-                dynamodb.getMappedPort(EXPOSED_PORT));
-        System.setProperty("aws.dynamo.endpoint", endpoint);
+                dynamodb.getMappedPort(8000));
+        registry.add("aws.dynamo.endpoint", () -> endpoint);
     }
+
+
 }
