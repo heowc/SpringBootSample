9add40a4690b16876c0e7793d9bba9e2a2e6afa0
heowc <heowc1992@gmail.com>
2020-05-23 21:28:51 +0900

    Fix due to graceful shutdown support

--- SpringBootGracefulShutdown/src/main/java/com/example/SpringBootGracefulShutdownApplication.java
+++ SpringBootGracefulShutdown/src/main/java/com/example/SpringBootGracefulShutdownApplication.java
@@ -1,26 +1,11 @@
 package com.example;
 
-import lombok.extern.log4j.Log4j2;
-import org.apache.catalina.connector.Connector;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class SpringBootGracefulShutdownApplication {
@@ -35,59 +20,12 @@ public class SpringBootGracefulShutdownApplication {
     }
 
     @RestController
-    public class LongProcessController {
+    public static class LongProcessController {
 
         @GetMapping
         public String pause() throws InterruptedException {
             Thread.sleep(5_000L);
             return "Process finished";
-        }
-
-    }
-
-    @Bean
-    public ConfigurableServletWebServerFactory webServerFactory(GracefulShutdown gracefulShutdown) {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.addConnectorCustomizers(gracefulShutdown);
-        return factory;
-    }
-
-    @Component
-    public static class GracefulShutdown implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
-
-        private static final Logger log = LoggerFactory.getLogger(GracefulShutdown.class);
-
-        private static final long TIMEOUT = 300_000L;
-
-        private volatile Connector connector;
-
-        @Override
-        public void customize(Connector connector) {
-            this.connector = connector;
-        }
-
-        @Override
-        public void onApplicationEvent(ContextClosedEvent event) {
-            this.connector.pause();
-            Executor executor = this.connector.getProtocolHandler().getExecutor();
-            if (executor instanceof ThreadPoolExecutor) {
-                try {
-                    ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
-                    threadPoolExecutor.shutdown();
-                    if (!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.MILLISECONDS)) {
-                        log.warn("Tomcat thread pool did not shut down gracefully within "
-                                + TIMEOUT + " seconds. Proceeding with forceful shutdown");
-
-                        threadPoolExecutor.shutdownNow();
-
-                        if (!threadPoolExecutor.awaitTermination(TIMEOUT, TimeUnit.MILLISECONDS)) {
-                            log.error("Tomcat thread pool did not terminate");
-                        }
-                    }
-                } catch (InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
-            }
         }
 
     }
