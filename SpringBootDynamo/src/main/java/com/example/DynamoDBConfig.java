914ba4f5929eb1476f2b79ba16083f117cda431d
heowc <heowc1992@gmail.com>
2022-04-13 23:35:01 +0900

    Upgrade aws-sdk-v2

--- SpringBootDynamo/src/main/java/com/example/DynamoDBConfig.java
+++ SpringBootDynamo/src/main/java/com/example/DynamoDBConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
@@ -21,6 +22,7 @@ public class DynamoDBConfig {
             @Value("${aws.access-key}") String accessKey,
             @Value("${aws.secret-key}") String secretKey) throws URISyntaxException {
         return DynamoDbClient.builder()
+                             .region(Region.of(region))
                              .credentialsProvider(StaticCredentialsProvider.create(
                                      AwsBasicCredentials.create(accessKey, secretKey))
                              )
