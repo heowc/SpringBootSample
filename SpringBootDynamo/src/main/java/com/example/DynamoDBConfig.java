ab718a351f547f8597024a14bb3c499eb508c993
heowc <heowc1992@gmail.com>
2022-04-13 23:24:26 +0900

    Upgrade aws-sdk-v2

--- SpringBootDynamo/src/main/java/com/example/DynamoDBConfig.java
+++ SpringBootDynamo/src/main/java/com/example/DynamoDBConfig.java
@@ -1,26 +1,30 @@
 package com.example;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class DynamoDBConfig {
 
     @Bean
-    public AmazonDynamoDB amazonDynamoDB(
+    public DynamoDbClient amazonDynamoDB(
             @Value("${aws.region}") String region,
             @Value("${aws.dynamo.endpoint}") String dynamoEndpoint,
             @Value("${aws.access-key}") String accessKey,
-            @Value("${aws.secret-key}") String secretKey) {
-        return AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(dynamoEndpoint, region))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
-                .build();
+            @Value("${aws.secret-key}") String secretKey) throws URISyntaxException {
+        return DynamoDbClient.builder()
+                             .credentialsProvider(StaticCredentialsProvider.create(
+                                     AwsBasicCredentials.create(accessKey, secretKey))
+                             )
+                             .endpointOverride(new URI(dynamoEndpoint))
+                             .build();
     }
 }
--- SpringBootDynamo/src/test/java/com/example/SpringBootDynamoApplicationTests.java
+++ SpringBootDynamo/src/test/java/com/example/SpringBootDynamoApplicationTests.java
@@ -1,149 +1,187 @@
 package com.example;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.model.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.DeleteTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
+import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
 
 public class SpringBootDynamoApplicationTests extends AbstractIntegrationTest {
 
     private static final String TABLE_NAME = "test";
 
     @Autowired
-    private AmazonDynamoDB dynamoDB;
+    private DynamoDbClient dynamoDB;
 
     @BeforeEach
     public void setup() {
-        dynamoDB.createTable(new CreateTableRequest()
-                .withTableName(TABLE_NAME)
-                .withAttributeDefinitions(Arrays.asList(
-                        new AttributeDefinition("id", ScalarAttributeType.S)
-                ))
-                .withKeySchema(Arrays.asList(
-                        new KeySchemaElement("id", KeyType.HASH)
-                ))
-                .withProvisionedThroughput(new ProvisionedThroughput(5L, 5L))
-        );
+        dynamoDB.createTable(
+                CreateTableRequest.builder()
+                                  .tableName(TABLE_NAME)
+                                  .attributeDefinitions(AttributeDefinition.builder()
+                                                                           .attributeName("id")
+                                                                           .attributeType(ScalarAttributeType.S)
+                                                                           .build())
+                                  .keySchema(KeySchemaElement.builder()
+                                                             .attributeName("id")
+                                                             .keyType(KeyType.HASH)
+                                                             .build())
+                                  .provisionedThroughput(ProvisionedThroughput.builder()
+                                                                              .readCapacityUnits(5L)
+                                                                              .writeCapacityUnits(5L)
+                                                                              .build())
+                                  .build());
     }
 
     @Test
     public void 아이템_삽입_후_스캔_데이터_확인_성공() {
         // given
         Map<String, AttributeValue> item = new HashMap<>();
-        item.put("id", new AttributeValue().withS("1"));
-        item.put("name", new AttributeValue().withS("wonchul"));
-        item.put("birth_year", new AttributeValue().withN("1992"));
+        item.put("id", AttributeValue.builder().s("1").build());
+        item.put("name", AttributeValue.builder().s("wonchul").build());
+        item.put("birth_year", AttributeValue.builder().n("1992").build());
 
         // when
-        dynamoDB.putItem(new PutItemRequest().withTableName(TABLE_NAME).withItem(item));
+        dynamoDB.putItem(PutItemRequest.builder()
+                                       .tableName(TABLE_NAME)
+                                       .item(item)
+                                       .build());
 
         // then
-        ScanResult result = dynamoDB.scan(new ScanRequest(TABLE_NAME));
-        assertThat(result.getCount()).isEqualTo(1);
-        assertThat(result.getItems().get(0).get("id").getS()).isEqualTo("1");
-        assertThat(result.getItems().get(0).get("name").getS()).isEqualTo("wonchul");
-        assertThat(result.getItems().get(0).get("birth_year").getN()).isEqualTo("1992");
+
+        ScanResponse response = dynamoDB.scan(ScanRequest.builder().tableName(TABLE_NAME).build());
+        assertThat(response.count()).isEqualTo(1);
+        assertThat(response.items().get(0).get("id").s()).isEqualTo("1");
+        assertThat(response.items().get(0).get("name").s()).isEqualTo("wonchul");
+        assertThat(response.items().get(0).get("birth_year").n()).isEqualTo("1992");
     }
 
     @Test
     public void 아이템_삽입_후_해당_데이터_검색_확인_성공() {
         // given
         Map<String, AttributeValue> item = new HashMap<>();
-        item.put("id", new AttributeValue().withS("1"));
-        item.put("name", new AttributeValue().withS("wonchul"));
-        item.put("birth_year", new AttributeValue().withN("1992"));
-        dynamoDB.putItem(new PutItemRequest().withTableName(TABLE_NAME).withItem(item));
+        item.put("id", AttributeValue.builder().s("1").build());
+        item.put("name", AttributeValue.builder().s("wonchul").build());
+        item.put("birth_year", AttributeValue.builder().n("1992").build());
+        dynamoDB.putItem(PutItemRequest.builder()
+                                       .tableName(TABLE_NAME)
+                                       .item(item)
+                                       .build());
 
         // when
         Map<String, AttributeValue> key = new HashMap<>();
-        key.put("id", new AttributeValue().withS("1"));
-        GetItemResult result = dynamoDB.getItem(new GetItemRequest().withTableName(TABLE_NAME).withKey(key));
+        key.put("id", AttributeValue.builder().s("1").build());
+        GetItemResponse response = dynamoDB.getItem(GetItemRequest.builder()
+                                                                  .tableName(TABLE_NAME)
+                                                                  .key(key)
+                                                                  .build());
 
         // then
-        assertThat(result).isNotNull();
-        assertThat(result.getItem()).isNotNull();
-        assertThat(result.getItem().get("id").getS()).isEqualTo("1");
-        assertThat(result.getItem().get("name").getS()).isEqualTo("wonchul");
-        assertThat(result.getItem().get("birth_year").getN()).isEqualTo("1992");
+        assertThat(response).isNotNull();
+        assertThat(response.item()).isNotNull();
+        assertThat(response.item().get("id").s()).isEqualTo("1");
+        assertThat(response.item().get("name").s()).isEqualTo("wonchul");
+        assertThat(response.item().get("birth_year").n()).isEqualTo("1992");
     }
 
     @Test
     public void 아이템_삽입_후_해당_데이터_수정_성공() {
         // given
         Map<String, AttributeValue> item = new HashMap<>();
-        item.put("id", new AttributeValue().withS("1"));
-        item.put("name", new AttributeValue().withS("wonchul"));
-        item.put("birth_year", new AttributeValue().withN("1992"));
-        dynamoDB.putItem(new PutItemRequest().withTableName(TABLE_NAME).withItem(item));
+        item.put("id", AttributeValue.builder().s("1").build());
+        item.put("name", AttributeValue.builder().s("wonchul").build());
+        item.put("birth_year", AttributeValue.builder().n("1992").build());
+        dynamoDB.putItem(PutItemRequest.builder()
+                                       .tableName(TABLE_NAME)
+                                       .item(item)
+                                       .build());
 
         // when
         Map<String, AttributeValue> updateKey = new HashMap<>();
-        updateKey.put("id", new AttributeValue().withS("1"));
+        updateKey.put("id", AttributeValue.builder().s("1").build());
 
         Map<String, String> nameMap = new HashMap<>();
         nameMap.put("#N", "name");
         nameMap.put("#BY", "birth_year");
 
         Map<String, AttributeValue> valueMap = new HashMap<>();
-        valueMap.put(":name", new AttributeValue().withS("WONCHUL"));
-        valueMap.put(":birth_year", new AttributeValue().withN("1992"));
-        dynamoDB.updateItem(
-                new UpdateItemRequest()
-                        .withTableName(TABLE_NAME)
-                        .withKey(updateKey)
-                        .withUpdateExpression("set #N = :name, #BY = :birth_year")
-                        .withExpressionAttributeNames(nameMap)
-                        .withExpressionAttributeValues(valueMap)
-        );
+        valueMap.put(":name", AttributeValue.builder().s("WONCHUL").build());
+        valueMap.put(":birth_year", AttributeValue.builder().n("1992").build());
+        dynamoDB.updateItem(UpdateItemRequest.builder()
+                                             .tableName(TABLE_NAME)
+                                             .key(updateKey)
+                                             .updateExpression("set #N = :name, #BY = :birth_year")
+                                             .expressionAttributeNames(nameMap)
+                                             .expressionAttributeValues(valueMap)
+                                             .build());
 
         // then
         Map<String, AttributeValue> key = new HashMap<>();
-        key.put("id", new AttributeValue().withS("1"));
-        GetItemResult result = dynamoDB.getItem(new GetItemRequest().withTableName(TABLE_NAME).withKey(key));
-
-        assertThat(result).isNotNull();
-        assertThat(result.getItem()).isNotNull();
-        assertThat(result.getItem()).isNotNull();
-        assertThat(result.getItem().get("id").getS()).isEqualTo("1");
-        assertThat(result.getItem().get("name").getS()).isEqualTo("WONCHUL");
-        assertThat(result.getItem().get("birth_year").getN()).isEqualTo("1992");
+        key.put("id", AttributeValue.builder().s("1").build());
+        GetItemResponse response = dynamoDB.getItem(GetItemRequest.builder()
+                                                                  .tableName(TABLE_NAME)
+                                                                  .key(key)
+                                                                  .build());
+        assertThat(response).isNotNull();
+        assertThat(response.item()).isNotNull();
+        assertThat(response.item().get("id").s()).isEqualTo("1");
+        assertThat(response.item().get("name").s()).isEqualTo("WONCHUL");
+        assertThat(response.item().get("birth_year").n()).isEqualTo("1992");
     }
 
     @Test
     public void 아이템_삽입_후_해당_데이터_삭제_성공() {
         // given
         Map<String, AttributeValue> item = new HashMap<>();
-        item.put("id", new AttributeValue().withS("1"));
-        item.put("name", new AttributeValue().withS("wonchul"));
-        item.put("birth_year", new AttributeValue().withN("1992"));
-        dynamoDB.putItem(new PutItemRequest().withTableName(TABLE_NAME).withItem(item));
+        item.put("id", AttributeValue.builder().s("1").build());
+        item.put("name", AttributeValue.builder().s("wonchul").build());
+        item.put("birth_year", AttributeValue.builder().n("1992").build());
+        dynamoDB.putItem(PutItemRequest.builder()
+                                       .tableName(TABLE_NAME)
+                                       .item(item)
+                                       .build());
 
         // when
         Map<String, AttributeValue> deleteKey = new HashMap<>();
-        deleteKey.put("id", new AttributeValue().withS("1"));
-        dynamoDB.deleteItem(new DeleteItemRequest().withTableName(TABLE_NAME).withKey(deleteKey));
+        deleteKey.put("id", AttributeValue.builder().s("1").build());
+        dynamoDB.deleteItem(DeleteItemRequest.builder().tableName(TABLE_NAME).key(deleteKey).build());
 
         // then
         Map<String, AttributeValue> key = new HashMap<>();
-        key.put("id", new AttributeValue().withS("1"));
-        GetItemResult result = dynamoDB.getItem(new GetItemRequest().withTableName(TABLE_NAME).withKey(key));
+        key.put("id", AttributeValue.builder().s("1").build());
+        GetItemResponse response = dynamoDB.getItem(GetItemRequest.builder()
+                                                                  .tableName(TABLE_NAME)
+                                                                  .key(key)
+                                                                  .build());
 
-        assertThat(result).isNotNull();
-        assertThat(result.getItem()).isNull();
+        assertThat(response).isNotNull();
+        assertThat(response.item()).isEmpty();
     }
-
 
     @AfterEach
     public void clear() {
-        dynamoDB.deleteTable("test");
+        dynamoDB.deleteTable(DeleteTableRequest.builder().tableName(TABLE_NAME).build());
     }
 
 }
